7bf985a4919b11001494e213bf5159352b62148d
davidabutler92 <davidabutler92@yahoo.com>
2020-04-07 11:04:53 -0700

    add debugging inputs for 1 and 2

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
+//UI Logic
 $(document).ready(function(){
   $("#numForm").submit(function(event) {
     event.preventDefault();
     const userInput = $("#userNumber").val();
+ 
 
-  
-    // buisness logic
-    // function(params) {} or params => {}
-    // Initialied a function that takes a user input
-    // how can I dynamically generate elements for my list??
-    const calNums =  userInput => {
-      const arr = [];
-      for(let i = 0; i <= userInput; i++) {
-        const arrInput = i;       
-        if(arrInput.toString().includes(1)) {
-          arr.push("beep");
-        } if(arrInput.toString().includes(2)) {
-            arr.push("boop");
-        } if(arrInput.toString().includes(3)) {
-            arr.push("wont you be my neighbor?");
-        } else {
-            arr.push(arrInput);
-        }
+
+
+// buisness logic
+// function(params) {} or params => {}
+// Initialied a function that takes a user input
+// how can I dynamically generate elements for my list??
+const calNums =  userInput => { 
+  const arr = []; 
+    for(let i = 0; i <= userInput; i++) {
+        arrInput = i;       
+      if(arrInput.toString().includes(1)) {
+        arr.push("beep");
+      } if(arrInput.toString().includes(2)) {
+          arr.push("boop");
+      } if(arrInput.toString().includes(3)) {
+          arr.push("wont you be my neighbor?");
+      } else {
+          arr.push(arrInput);
       }
-      console.log(arr);
-      // returning the array after looping
-      return arr;
     }
+    // console.log(arr);
+    // returning the array after looping
+    return arr;
+}
     const userResults = calNums(userInput);
     $("#results").text(userResults);
+ 
+
   });
 });
-
